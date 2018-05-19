17d98ab54e0e4c2a41f5df46bf496ee2b90683fd
agungdh <agungdh@live.com>
2018-05-19 11:20:19 +0700

    update dari meng

---
+++ Aplikasi_Pengumpulan_Data_Pengajuan.mm
@@ -0,0 +1,277 @@
+<map version="freeplane 1.5.9">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Aplikasi Pengumpulan Data Pengajuan Borang Akreditasi Berbasis Web di Universitas XYZ" FOLDED="false" ID="ID_476111110" CREATED="1526527856124" MODIFIED="1526528112380" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle" zoom="0.751">
+    <properties fit_to_viewport="false;"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+<edge COLOR="#ff0000"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+<edge COLOR="#0000ff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+<edge COLOR="#00ff00"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+<edge COLOR="#ff00ff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5">
+<edge COLOR="#00ffff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6">
+<edge COLOR="#7c0000"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7">
+<edge COLOR="#00007c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8">
+<edge COLOR="#007c00"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9">
+<edge COLOR="#7c007c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10">
+<edge COLOR="#007c7c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11">
+<edge COLOR="#7c7c00"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="9" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="I. PENDAHULUAN" POSITION="right" ID="ID_217394046" CREATED="1526528180806" MODIFIED="1526528228253">
+<edge COLOR="#ff0000"/>
+<node TEXT="Latar Belakang" ID="ID_538032735" CREATED="1526528297901" MODIFIED="1526528339315">
+<node TEXT="Paragraf 1" ID="ID_36948605" CREATED="1526528314125" MODIFIED="1526528347775">
+<node TEXT="Profil Universitas" ID="ID_906599683" CREATED="1526528538844" MODIFIED="1526528549356"/>
+</node>
+<node TEXT="Paragraf 2" ID="ID_1853436677" CREATED="1526528521535" MODIFIED="1526528526435">
+<node TEXT="Pentingnya akreditasi" ID="ID_254117182" CREATED="1526528643444" MODIFIED="1526528652482"/>
+</node>
+<node TEXT="Paragraf 3" ID="ID_246315791" CREATED="1526528527655" MODIFIED="1526528537637">
+<node TEXT="Permasalahan sistem yang berjalan" ID="ID_461835229" CREATED="1526528657660" MODIFIED="1526700527055"/>
+</node>
+<node TEXT="Paragraf 4" ID="ID_13262656" CREATED="1526528689020" MODIFIED="1526528728826">
+<node TEXT="Solusi dengan membuat aplikasi" ID="ID_629740472" CREATED="1526528730493" MODIFIED="1526700538234"/>
+</node>
+</node>
+<node TEXT="Tujuan" ID="ID_376117823" CREATED="1526528326561" MODIFIED="1526528369393">
+<node TEXT="Menghasilkan Aplikasi Pengumpulan Data Pengajuan Borang Akreditasi Berbasis Web di Universitas XYZ guna untuk mempermudah staf dalam pengajuan akreditasi" ID="ID_1285102606" CREATED="1526528780251" MODIFIED="1526528904567"/>
+</node>
+<node TEXT="Kerangka Pemikian" ID="ID_1237127539" CREATED="1526528370739" MODIFIED="1526697026296">
+<node TEXT="Permasalahan" ID="ID_57875930" CREATED="1526697031587" MODIFIED="1526697073576">
+<node TEXT="Teknik pengumpulan data pengajuan akreditasi masih dilakukan dengan menggunakan perantara flashdisk atau email dan proses pengajuan akreditasi nya pun masih dilakukan dengan cara staf harus mendatangi DPM (Direktorat Pengajuan Mutu) secara langsung sehingga menyebabkan proses input dan output menjadi lama dan tidak efisiean karena manajemen penyimpanan tidak tersusun rapi" ID="ID_622874155" CREATED="1526701781111" MODIFIED="1526701785246"/>
+</node>
+<node TEXT="Solusi" ID="ID_1229328343" CREATED="1526697075542" MODIFIED="1526697079736">
+<node TEXT="Membuat aplikasi pengajuan borang akreditasi di universitas xyz untuk mempercepat proses pengumpulan dan penilaian dokumen sebelum diserahka kepada BAN PT" ID="ID_1584541691" CREATED="1526701796767" MODIFIED="1526701800177"/>
+</node>
+<node TEXT="Pengembangan aplikasi" ID="ID_1609429079" CREATED="1526697086172" MODIFIED="1526697095703">
+<node TEXT="Metode Pengembangan" ID="ID_1434900367" CREATED="1526701805451" MODIFIED="1526701810637">
+<node TEXT="" ID="ID_1228954332" CREATED="1526702285904" MODIFIED="1526702285904">
+<node ID="ID_1359328301" CREATED="1526702297031" MODIFIED="1526702297031"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Rapid Application Development (RAD)
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+</node>
+<node TEXT="" ID="ID_1628803275" CREATED="1526702299332" MODIFIED="1526702299332">
+<node ID="ID_868459038" CREATED="1526702315981" MODIFIED="1526702315981"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Pemodelan Bisnis
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<node TEXT="Wawancara" ID="ID_1276483924" CREATED="1526703242602" MODIFIED="1526703267096"/>
+</node>
+</node>
+<node TEXT="" ID="ID_1294764255" CREATED="1526702303648" MODIFIED="1526702303648">
+<node ID="ID_55534470" CREATED="1526702336210" MODIFIED="1526702336210"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Pmodelan Proses
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<node TEXT="Sequence Diagram" ID="ID_1673234655" CREATED="1526703246579" MODIFIED="1526703290512"/>
+<node TEXT="Flowchart" ID="ID_1169278070" CREATED="1526703282246" MODIFIED="1526703297067"/>
+<node TEXT="Interface" ID="ID_1174347772" CREATED="1526703283193" MODIFIED="1526703314675"/>
+</node>
+</node>
+<node TEXT="" ID="ID_1962358480" CREATED="1526702338303" MODIFIED="1526702338303">
+<node ID="ID_1946326820" CREATED="1526702827571" MODIFIED="1526702827571"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Pembuatan Aplikasi
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<node TEXT="PHP" ID="ID_592411633" CREATED="1526702871590" MODIFIED="1526702880146"/>
+<node TEXT="HTML" ID="ID_1399648418" CREATED="1526702873191" MODIFIED="1526702883525"/>
+<node TEXT="MySQL" ID="ID_455467181" CREATED="1526702874126" MODIFIED="1526702888470"/>
+<node TEXT="JavaScript" ID="ID_1711237622" CREATED="1526702875106" MODIFIED="1526702893726"/>
+</node>
+</node>
+<node TEXT="" ID="ID_1945997965" CREATED="1526702341866" MODIFIED="1526702341866">
+<node ID="ID_123077210" CREATED="1526702836147" MODIFIED="1526703315871"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Pengujian dan Pergantian
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<node TEXT="Black-Box Testing" ID="ID_44329796" CREATED="1526703325244" MODIFIED="1526703338696"/>
+</node>
+</node>
+</node>
+<node TEXT="Hasil" ID="ID_452829985" CREATED="1526697097307" MODIFIED="1526697100911"/>
+</node>
+<node TEXT="Kontribusi" ID="ID_1149591746" CREATED="1526528387434" MODIFIED="1526696989621">
+<node TEXT="Meningkatkan efesiensi waktu pengumpulan data pengajuan borang akreditasi" ID="ID_413814151" CREATED="1526703355758" MODIFIED="1526703433244"/>
+<node TEXT="" ID="ID_716483753" CREATED="1526703375943" MODIFIED="1526703375943">
+<node ID="ID_678562347" CREATED="1526703395605" MODIFIED="1526703411058"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p class="MsoListParagraph" style="text-align: justify; text-indent: -18.0pt; line-height: 200%">
+      b. Manajemen file yang tertata rapi membuat penilaian secara internal lebih mudah sebelum diserahkan ke BAN-PT
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+<node TEXT="" ID="ID_818574494" CREATED="1526703378355" MODIFIED="1526703378355">
+<node ID="ID_1028657595" CREATED="1526703422474" MODIFIED="1526703422474"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <span lang="IN" style="font-size: 12.0pt; line-height: 115%; font-family: Times New Roman,serif"><font size="12.0pt" face="Times New Roman,serif">Penilaian data secara langsung mempermudah pihak pengajuan untuk memperbaiki kekurangan data sebelumnya</font></span>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+<node TEXT="" ID="ID_1246023698" CREATED="1526703379362" MODIFIED="1526703379362">
+<node ID="ID_1697609703" CREATED="1526703428792" MODIFIED="1526703428792"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p class="MsoListParagraph" style="text-align: justify; text-indent: -18.0pt; line-height: 200%">
+      <span><font size="12.0pt" face="Times New Roman,serif">a.</font></span><span style="font-style: normal; font-variant: normal; font-weight: normal; font-size: 7.0pt; line-height: normal; font-family: Times New Roman"><font size="7.0pt" face="Times New Roman">&#160;&#160;&#160;&#160;&#160;&#160; </font></span><span lang="IN" style="font-size: 12.0pt; line-height: 200%; font-family: Times New Roman,serif"><font size="12.0pt" face="Times New Roman,serif">Pihak pengajuan dapat melihat presentase file yang telah diajukan sehingga mempermudah</font></span><font size="12.0pt" face="Times New Roman,serif"><span>&#160; </span><span lang="IN" style="font-size: 12.0pt; line-height: 200%; font-family: Times New Roman,serif">pemantauan dokumen yang telah dikumpulkan.</span></font>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="II. TINJAUAN PUSTAKA" POSITION="right" ID="ID_958783568" CREATED="1526528200269" MODIFIED="1526528243616">
+<edge COLOR="#00ff00"/>
+</node>
+<node TEXT="III. METODE PELAKSANAAN" POSITION="right" ID="ID_1699775042" CREATED="1526528202725" MODIFIED="1526528254814">
+<edge COLOR="#00ffff"/>
+</node>
+<node TEXT="IV. HASIL DAN PEMBAHASAN" POSITION="right" ID="ID_1472507902" CREATED="1526528204792" MODIFIED="1526528279628">
+<edge COLOR="#00007c"/>
+</node>
+<node TEXT="V. KESIMPULAN DAN SARAN" POSITION="right" ID="ID_472810413" CREATED="1526528207322" MODIFIED="1526528295515">
+<edge COLOR="#7c007c"/>
+</node>
+</node>
+</map>
